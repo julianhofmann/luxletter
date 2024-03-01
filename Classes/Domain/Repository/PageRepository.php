b4c1de73ceb844891fae9af790d5d4549b674c76
Alexander Kellner <alexander.kellner@in2code.de>
2024-03-01 17:38:12 +0100

    [FEATURE] Respect access rights for newsletter selection in options in new/edit view
    Related: https://github.com/in2code-de/luxletter/pull/211
    
    (cherry picked from commit fc2323f75d87c60b7cafb38f0f36bf6828abd358)

--- Classes/Domain/Repository/PageRepository.php
+++ Classes/Domain/Repository/PageRepository.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace In2code\Luxletter\Domain\Repository;
 
 use Doctrine\DBAL\Driver\Exception as ExceptionDbalDriver;
+use In2code\Luxletter\Domain\Service\PermissionTrait;
 use In2code\Luxletter\Exception\MisconfigurationException;
+use In2code\Luxletter\Utility\BackendUserUtility;
 use In2code\Luxletter\Utility\ConfigurationUtility;
 use In2code\Luxletter\Utility\DatabaseUtility;
 use PDO;
@@ -12,11 +14,10 @@ use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotCon
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Utility\MathUtility;
 
-/**
- * Class PageRepository
- */
 class PageRepository
 {
+    use PermissionTrait;
+
     const TABLE_NAME = 'pages';
 
     /**
@@ -27,58 +28,66 @@ class PageRepository
      *  ]
      *
      * @return array
-     * @throws ExceptionDbalDriver
      */
     public function findAllNewsletterPages(): array
     {
         $pages = [];
-        $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
-        $results = $queryBuilder
-            ->select('*')
-            ->from(self::TABLE_NAME)
-            ->where(
-                'doktype=' . \In2code\Luxletter\Utility\ConfigurationUtility::getMultilanguageNewsletterPageDoktype()
-                . ' and sys_language_uid=0'
-            )
-            ->orderBy('title', 'desc')
-            ->execute()
-            ->fetchAllAssociative();
-        foreach ($results as $result) {
-            $pages[$result['uid']] = $result['title'];
+        try {
+            $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
+            $rows = $queryBuilder
+                ->select('*')
+                ->from(self::TABLE_NAME)
+                ->where(
+                    'doktype=' . ConfigurationUtility::getMultilanguageNewsletterPageDoktype()
+                    . ' and sys_language_uid=0'
+                )
+                ->orderBy('title', 'asc')
+                ->executeQuery()
+                ->fetchAllAssociative();
+            if (BackendUserUtility::isAdministrator() === false) {
+                foreach ($rows as $key => $row) {
+                    if ($this->isAuthenticated($row) === false) {
+                        unset($rows[$key]);
+                    }
+                }
+            }
+            foreach ($rows as $row) {
+                $pages[$row['uid']] = $row['title'];
+            }
+        } catch (Throwable $exception) {
+            unset($exception);
         }
         return $pages;
     }
 
-    /**
-     * @param int $pageIdentifier
-     * @param int $language
-     * @return string
-     * @throws ExceptionDbalDriver
-     */
     public function getSubjectFromPageIdentifier(int $pageIdentifier, int $language): string
     {
-        $fieldname = 'uid';
-        if ($language > 0) {
-            $fieldname = 'l10n_parent';
-        }
-        $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
-        $subject = $queryBuilder
-            ->select('luxletter_subject')
-            ->from(self::TABLE_NAME)
-            ->where(
-                $queryBuilder->expr()->eq(
-                    $fieldname,
-                    $queryBuilder->createNamedParameter($pageIdentifier, PDO::PARAM_INT)
-                ),
-                $queryBuilder->expr()->eq(
-                    'sys_language_uid',
-                    $queryBuilder->createNamedParameter($language, PDO::PARAM_INT)
+        try {
+            $fieldname = 'uid';
+            if ($language > 0) {
+                $fieldname = 'l10n_parent';
+            }
+            $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
+            $subject = $queryBuilder
+                ->select('luxletter_subject')
+                ->from(self::TABLE_NAME)
+                ->where(
+                    $queryBuilder->expr()->eq(
+                        $fieldname,
+                        $queryBuilder->createNamedParameter($pageIdentifier, PDO::PARAM_INT)
+                    ),
+                    $queryBuilder->expr()->eq(
+                        'sys_language_uid',
+                        $queryBuilder->createNamedParameter($language, PDO::PARAM_INT)
+                    )
                 )
-            )
-            ->execute()
-            ->fetchOne();
-        /** @var string $subject */
-        return $subject;
+                ->executeQuery()
+                ->fetchOne();
+            /** @var string $subject */
+            return $subject;
+        } catch (Throwable $exception) {
+            return '';
+        }
     }
 
     /**
@@ -88,7 +97,6 @@ class PageRepository
      * @return array
      * @throws ExtensionConfigurationExtensionNotConfiguredException
      * @throws ExtensionConfigurationPathDoesNotExistException
-     * @throws ExceptionDbalDriver
      * @throws MisconfigurationException
      */
     public function getLanguagesFromOrigin(string $origin): array
@@ -102,40 +110,38 @@ class PageRepository
         return [0];
     }
 
-    /**
-     * @param int $pageIdentifier
-     * @return array
-     * @throws ExceptionDbalDriver
-     */
     protected function getLanguagesFromPageIdentifier(int $pageIdentifier): array
     {
-        $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
-        $languages = $queryBuilder
-            ->select('sys_language_uid')
-            ->from(self::TABLE_NAME)
-            ->where('l10n_parent=' . (int)$pageIdentifier)
-            ->execute()
-            ->fetchFirstColumn();
-        if ($this->isDefaultLanguageEnabled($pageIdentifier)) {
-            $languages = array_merge([0], $languages);
+        try {
+            $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
+            $languages = $queryBuilder
+                ->select('sys_language_uid')
+                ->from(self::TABLE_NAME)
+                ->where('l10n_parent=' . $pageIdentifier)
+                ->executeQuery()
+                ->fetchFirstColumn();
+            if ($this->isDefaultLanguageEnabled($pageIdentifier)) {
+                $languages = array_merge([0], $languages);
+            }
+            return $languages;
+        } catch (Throwable $exception) {
+            return [];
         }
-        return $languages;
     }
 
-    /**
-     * @param int $pageIdentifier
-     * @return bool
-     * @throws ExceptionDbalDriver
-     */
     protected function isDefaultLanguageEnabled(int $pageIdentifier): bool
     {
-        $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
-        $languageConfiguration = $queryBuilder
-            ->select('l18n_cfg')
-            ->from(self::TABLE_NAME)
-            ->where('uid=' . (int)$pageIdentifier)
-            ->execute()
-            ->fetchOne();
-        return $languageConfiguration !== 1 && $languageConfiguration !== 3;
+        try {
+            $queryBuilder = DatabaseUtility::getQueryBuilderForTable(self::TABLE_NAME);
+            $languageConfiguration = $queryBuilder
+                ->select('l18n_cfg')
+                ->from(self::TABLE_NAME)
+                ->where('uid=' . (int)$pageIdentifier)
+                ->executeQuery()
+                ->fetchOne();
+            return $languageConfiguration !== 1 && $languageConfiguration !== 3;
+        } catch (Throwable $exception) {
+            return true;
+        }
     }
 }
