550264f40ee196d059cfbf4c723c24af19485f13
Alexander Kellner <alexander.kellner@in2code.de>
2024-03-07 10:30:35 +0100

    [FEATURE] Respect time filter in dashboard
    (cherry picked from commit 7b630d95caae4f52884f4a1d7867a664ae98b672)

--- Classes/Controller/NewsletterController.php
+++ Classes/Controller/NewsletterController.php
@@ -48,6 +48,9 @@ class NewsletterController extends AbstractNewsletterController
     {
         $this->view->assignMultiple(
             [
+                'filter' => $filter,
+                'newsletters' => $this->newsletterRepository->findAllByFilter($filter->setLimit(10)),
+                'groupedLinksByHref' => $this->logRepository->getGroupedLinksByHref($filter->setLimit(8)),
                 'statistic' => [
                     'overallReceivers' => $this->logRepository->getNumberOfReceivers($filter),
                     'overallOpenings' => $this->logRepository->getOverallOpenings($filter),
@@ -59,9 +62,6 @@ class NewsletterController extends AbstractNewsletterController
                     'overallClickRate' => $this->logRepository->getOverallClickRate($filter),
                     'overallUnsubscribeRate' => $this->logRepository->getOverallUnsubscribeRate($filter),
                 ],
-                'filter' => $filter,
-                'groupedLinksByHref' => $this->logRepository->getGroupedLinksByHref($filter->setLimit(8)),
-                'newsletters' => $this->newsletterRepository->findAllAuthorized($filter->setLimit(10)),
             ]
         );
 
